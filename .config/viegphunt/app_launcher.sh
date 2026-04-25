4033eaa4dcb7737e899dea57a368d57730367c05
post7794 <post7794@users.noreply.github.com>
2026-04-26 02:54:13 +0800

    删除 zh_launcher.sh，rofi 已支持中文输入

--- .config/viegphunt/app_launcher.sh
+++ .config/viegphunt/app_launcher.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
 
-if [ "$1" = "--zh" ]; then
-    tmpfile=$(mktemp)
-    ghostty -e ~/.config/viegphunt/zh_launcher.sh > "$tmpfile" 2>/dev/null
-    selected=$(cat "$tmpfile")
-    rm -f "$tmpfile"
-    [ -z "$selected" ] && exit 0
-    gtk-launch "$selected" &
-else
-    if pidof rofi > /dev/null; then
-        pkill rofi
-    fi
-    ~/.config/viegphunt/rofi.sh -show drun
+if pidof rofi > /dev/null; then
+    pkill rofi
 fi
+
+~/.config/viegphunt/rofi.sh -show drun
